6d0e12afbf7639555503c50c112e64fac0f3c588
Ditlexd <ditlef.diseth@gmail.com>
2022-04-16 18:24:15 +0200

    Update config
    What: Setup tailwind, install prettier and create some files

---
+++ src/__tests__/lib/grid.test.tsx
@@ -0,0 +1,64 @@
+import { Grid, Robot } from "../../@types/simulator";
+import { place } from "../lib/grid";
+
+function getCleanGrid(): Grid {
+  return [
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+  ];
+}
+
+describe("PLACE on grid", () => {
+  test("Should place robot on correct grid position", () => {
+    const initialGrid = getCleanGrid();
+    const robot: Robot = { direction: "NORTH" };
+
+    const newGrid = place(robot, initialGrid, 2, 2, "NORTH");
+
+    expect(newGrid[2][2]).not.toBeNull();
+  });
+
+  test("Should move robot to new position", () => {
+    const initialGrid = getCleanGrid();
+    const robot: Robot = { direction: "NORTH" };
+
+    const newGrid = place(robot, initialGrid, 2, 2, "NORTH");
+    expect(newGrid[2][2]).not.toBeNull();
+
+    const nextGrid = place(robot, initialGrid, 0, 0, "NORTH");
+    expect(nextGrid[2][2]).toBeNull();
+    expect(nextGrid[0][0]).not.toBeNull();
+  });
+
+  test("Should give robot correct direciton", () => {
+    const initialGrid = getCleanGrid();
+    const robot: Robot = { direction: "NORTH" };
+
+    const north = place(robot, initialGrid, 2, 2, "NORTH");
+    expect(north[2][2]?.direction).toMatch("NORTH");
+
+    const east = place(robot, north, 2, 2, "EAST");
+    expect(east[2][2]?.direction).toMatch("EAST");
+
+    const south = place(robot, east, 2, 2, "SOUTH");
+    expect(south[2][2]?.direction).toMatch("SOUTH");
+
+    const west = place(robot, south, 2, 2, "WEST");
+    expect(west[2][2]?.direction).toMatch("WEST");
+  });
+
+  test("Should not do anything when trying to place out of bounds", () => {
+    const initialGrid = getCleanGrid();
+    const robot: Robot = { direction: "NORTH" };
+
+    const newGrid = place(robot, initialGrid, 2, 2, "NORTH");
+
+    expect(newGrid[2][2]).not.toBeNull();
+
+    const outOfBounds = place(robot, newGrid, 5, 2, "NORTH");
+    expect(outOfBounds[2][2]).not.toBeNull();
+  });
+});
